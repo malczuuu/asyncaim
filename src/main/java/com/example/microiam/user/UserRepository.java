7c759d7f8b3f5358c2d767beace39c526da14584
Damian Malczewski <damian.m.malczewski@gmail.com>
2023-03-05 22:10:12 +0100

    Add UserRepository

---
+++ src/main/java/com/example/microiam/user/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.microiam.user;
+
+import java.util.Optional;
+import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<UserEntity, ObjectId> {
+
+  Page<UserEntity> findAllByUsername(String username, Pageable pageable);
+
+  Optional<UserEntity> findByUuid(String uuid);
+
+  Optional<UserEntity> findByKeycloakId(String keycloakId);
+
+  Optional<UserEntity> findByStatusOrderByIdAsc(String status);
+}
